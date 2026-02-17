e236e83373a16e9d2519ba0a2a4b2e11c013facd
Phạm Tuấn Kiệt <167632480+TuanKiet1774@users.noreply.github.com>
2026-02-18 00:26:06 +0700

    Thêm auth

--- src/app.js
+++ src/app.js
@@ -21,11 +21,11 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/roles', roleRoutes);
-app.use('/api/role-sessions', roleSessionRoutes);
-app.use('/api/members', memberRoutes); // Yêu cầu đăng nhập để xem/quản lý thành viên
-app.use('/api/sessions', sessionRoutes); // Yêu cầu đăng nhập để xem/quản lý buổi sinh hoạt
-app.use('/api/attendance', attendanceRoutes); // Yêu cầu đăng nhập để điểm danh
+app.use('/api/roles', auth, roleRoutes);
+app.use('/api/role-sessions', auth, roleSessionRoutes);
+app.use('/api/members', auth, memberRoutes); // Yêu cầu đăng nhập để xem/quản lý thành viên
+app.use('/api/sessions', auth, sessionRoutes); // Yêu cầu đăng nhập để xem/quản lý buổi sinh hoạt
+app.use('/api/attendance', auth, attendanceRoutes); // Yêu cầu đăng nhập để điểm danh
 
 app.get('/', (req, res) => {
     res.send('Website Quản lý CLB API is running...');
